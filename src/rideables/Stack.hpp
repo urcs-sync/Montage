bc47814cf8ddcaa7b2ad70132050c3853d54bb3f
Shreif Elsaid <selsaid@node1x10a.cs.rochester.edu>
2020-12-13 15:39:39 -0500

    accessing atomic by loading instead of direct access

--- src/rideables/Stack.hpp
+++ src/rideables/Stack.hpp
@@ -2,72 +2,76 @@
 // #define STACK_HPP
 
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 #include <atomic>
 
 using namespace std;
 
 class Stack
 {
-    private:
+private:
 
     struct StackNode
     {
 
         int data;
         StackNode *next;
-
     };
 
-    atomic<StackNode*> top;
+    atomic<StackNode *> top;
 
-    public:
-    Stack()
+public:
+
+    Stack() : top(nullptr) {}
+
+    void push(int data)
     {
-        top = NULL;
-    }
 
-    void push(int data){
-
-        StackNode* new_node = new StackNode;
+        StackNode *new_node = new StackNode;
         new_node->data = data;
         new_node->next = NULL;
 
-        StackNode* old_node;
+        StackNode *old_node;
 
-        do {
-            old_node = top;
+        do
+        {
+            old_node = top.load();
             new_node->next = old_node;
-        } while (!top.compare_exchange_strong(old_node, new_node));
-
-
+        } while (!top.compare_exchange_weak(old_node, new_node));
     }
-    
-    int pop(){
-        StackNode* new_node;
-        StackNode* old_node;
-        do {
-            old_node = top;
-            if (old_node == NULL){
+
+    int pop()
+    {
+        StackNode *new_node;
+        StackNode *old_node;
+        do
+        {
+            old_node = top.load();
+            if (old_node == NULL)
+            {
                 return NULL;
             }
             new_node = old_node->next;
-        } while (!top.compare_exchange_strong(old_node, new_node));
+        } while (!top.compare_exchange_weak(old_node, new_node));
         return old_node->data;
     }
-    int peek(){
-        if(!is_empty()){
-            StackNode* top_node;
-            top_node = top;
+    int peek()
+    {
+        if (!is_empty())
+        {
+            StackNode *top_node;
+            top_node = top.load();
             return top_node->data;
-        }else{
+        }
+        else
+        {
             //TODO : throw errro
             return 55555;
         }
-
     }
 
-    bool is_empty(){
-        return top == NULL;
+    bool is_empty()
+    {
+        return top.load() == NULL;
     }
 };
