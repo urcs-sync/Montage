3c11378d8d0421877595ad1f4eda5a9f68bbbabf
Shreif Elsaid <selsaid@node1x10a.cs.rochester.edu>
2020-12-14 21:21:27 -0500

    adding stack base class

---
+++ src/RStack.hpp
@@ -0,0 +1,16 @@
+#ifndef RSTACK_HPP
+#define RSTACK_HPP
+​
+#include "Rideable.hpp"
+​
+#include "optional.hpp"
+​
+template <class V> class RStack : public virtual Rideable{
+public:
+​
+    virtual optional<V> pop(int tid)=0;
+​
+    virtual void push(V val, int tid)=0;
+};
+​
+#endif
