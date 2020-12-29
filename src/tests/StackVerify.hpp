fe0705c738b3152a3ad732a20fcf1e40fb89719c
Wentao Cai <wcai6@cs.rochester.edu>
2020-12-28 23:57:04 -0500

    convert pair to string in StackVerify

--- src/tests/StackVerify.hpp
+++ src/tests/StackVerify.hpp
@@ -11,41 +11,44 @@
 #include <unordered_map>
 
 
-template <typename T>
 class StackVerify : public Test{
+    const int TID_SIZE=4;
+    const int CNT_SIZE=60;
+public:
+    using T=std::string;
+    int prop_push, prop_pop;
+    static inline thread_local uint64_t counter = 0;
+    RStack<T>* s;
+    std::unordered_map<int, uint64_t> map; 
+    StackVerify(int p_push, int p_pop) : prop_push(p_push), prop_pop(p_pop){}
+    void operation(int op, int tid);
+    void init(GlobalTestConfig* gtc);
+    void parInit(GlobalTestConfig* gtc, LocalTestConfig* ltc);
+    int execute(GlobalTestConfig* gtc, LocalTestConfig* ltc);
+    void verify();
+    void cleanup(GlobalTestConfig* gtc);
 
-    public :
-        int prop_push, prop_pop;
-        static thread_local int counter;
-        RStack<T>* s;
-        std::unordered_map<int, int> map; 
-        StackVerify(int p_push, int p_pop) : prop_push(p_push), prop_pop(p_pop){}
-        void operation(int op, int tid);
-        void init(GlobalTestConfig* gtc);
-        void parInit(GlobalTestConfig* gtc, LocalTestConfig* ltc);
-        int execute(GlobalTestConfig* gtc, LocalTestConfig* ltc);
-        void verify(int tid);
-        void cleanup(GlobalTestConfig* gtc);
-
+    inline T toString(int tid, uint64_t v){
+        auto _tid = std::to_string(tid);
+        assert(_tid.size()<=TID_SIZE);
+        auto _v = std::to_string(v);
+        assert(_v.size()<=CNT_SIZE);
+        return std::string(TID_SIZE-_tid.size(),'0')+_tid+std::string(CNT_SIZE-_v.size(),'0')+_v;
+    }
 };
 
-template <typename T>
-thread_local int StackVerify<T>::counter = 0;
-
-template <typename T>
-void StackVerify<T>::parInit(GlobalTestConfig* gtc, LocalTestConfig* ltc){
+void StackVerify::parInit(GlobalTestConfig* gtc, LocalTestConfig* ltc){
     s->init_thread(gtc, ltc);
 }
 
 
-template <typename T>
-void StackVerify<T>::init(GlobalTestConfig* gtc){
+void StackVerify::init(GlobalTestConfig* gtc){
     if (gtc->task_num <= 1){
         errexit("StackVerify must run on multiple threads.");
     }
 
     Rideable* ptr = gtc->allocRideable();
-    s = dynamic_cast<RStack<pair<int, int>>*>(ptr);
+    s = dynamic_cast<RStack<T>*>(ptr);
     if (!s) {
         errexit("StackVerify must be run on RStack<T> type object.");
     }
@@ -56,23 +59,17 @@ void StackVerify<T>::init(GlobalTestConfig* gtc){
 
 }
 
-template <typename T>
-void StackVerify<T>::operation(int op, int tid){
+void StackVerify::operation(int op, int tid){
     if(op < this->prop_push){
-        pair<int,int> value;
-        value.first = tid;
-        value.second = counter;
-        s->push(value, tid);
+        s->push(toString(tid,counter), tid);
         counter++;
     }
     else{
        s->pop(tid);
-
     }
 }
 
-template <typename T>
-int StackVerify<T>::execute(GlobalTestConfig* gtc, LocalTestConfig* ltc){
+int StackVerify::execute(GlobalTestConfig* gtc, LocalTestConfig* ltc){
     auto time_up = gtc->finish;
     counter = 0;
     int ops = 0;
@@ -96,32 +93,31 @@ int StackVerify<T>::execute(GlobalTestConfig* gtc, LocalTestConfig* ltc){
     return ops;
 }
 
-template <typename T>
-void StackVerify<T>::verify(int tid){
+void StackVerify::verify(){
+    // called only by main thread
     while(!s->is_empty()){
-        auto popped_val = s->pop(tid);
+        auto popped_val = s->pop(0);
         if(popped_val.has_value()){
-            int tid = popped_val.value().first;
-            int monoval = popped_val.value().second;
+            int tid = std::stoi(popped_val.value().substr(0,TID_SIZE));
+            uint64_t monoval = std::stoull(popped_val.value().substr(TID_SIZE));
 
             if (map.find(tid) != map.end()) {
                 if(map[tid] < monoval){
-                    std::cout<<"tid: "<<tid<<"\n --- last value popped for this tid : "<<map[tid]<<"\n --- current value popped: "<<monoval<<"\n --- not verfied."<<std::endl;
+                    std::cerr<<"tid: "<<tid<<"\n --- last value popped for this tid : "<<map[tid]<<"\n --- current value popped: "<<monoval<<"\n --- not verfied."<<std::endl;
+                    exit(1);
                 } else {
                     map[tid] = monoval;
                 }
             } else {
                 map[tid] = monoval;
             }
-
         }
     }
+    std::cout<<"Verified!"<<std::endl;
 }
 
-template <typename T>
-void StackVerify<T>::cleanup(GlobalTestConfig* gtc){
-    verify(0);
-
+void StackVerify::cleanup(GlobalTestConfig* gtc){
+    verify();
     delete s;
 }
 
