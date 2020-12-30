330d9a105203145d3d8a983c1f27b69027036a63
rvs314 <rvs314@protonmail.com>
2020-12-29 23:04:14 -0500

    Basic Outline - started =get=

--- src/rideables/CLevelHashTable.hpp
+++ src/rideables/CLevelHashTable.hpp
@@ -32,8 +32,7 @@ const int epoch_freq = 100;
 const int empty_freq = 1000;
 const bool collect = true;
 
-template <typename K, typename V, typename Hasher = std::hash<K>>
-class CLevelHashTable : public RMap<K, V>, public Recoverable {
+template <typename K, typename V> class CLevelHashTable : public RMap<K, V> {
   class Payload : public pds::PBlk {
     GENERATE_FIELD(K, key, Payload);
     GENERATE_FIELD(V, val, Payload);
@@ -55,7 +54,8 @@ class CLevelHashTable : public RMap<K, V>, public Recoverable {
   public:
     PayloadRef(Payload *plp, size_t hash) {
       uint16_t tag = static_cast<uint16_t>(hash & 0xFFFF);
-      this->data = static_cast<uint64_t>((static_cast<uint64_t>(tag) << 48) | plp);
+      this->data =
+          static_cast<uint64_t>((static_cast<uint64_t>(tag) << 48) | plp);
     }
 
     // Returns the first 16 bits of the payload
@@ -100,12 +100,42 @@ public:
       : tracker{task_number, epoch_freq, empty_freq, collect} {}
 
   optional<V> get(K key, int tid) {
+    std::hash<K> hasher{};
+    uint64_t hash = hasher(key);
+
+    uint32_t top = static_cast<uint32_t>(hash >> 32);
+    uint32_t bottom = static_cast<uint32_t>(hash & 0xffffffff);
+    return optional<V>{};
+  }
+
+  optional<V> put(K key, V val, int tid) {
+    errexit("Put not implemented");
+    return optional<V>{};
+  }
+
+  bool insert(K key, V val, int tid) {
+    errexit("Insert not implemented");
+    return false;
+  }
+
+  optional<V> remove(K key, int tid) {
+    errexit("Remove not implemented");
+    return optional<V>{};
+  }
+
+  optional<V> replace(K key, V val, int tid) {
+    errexit("Replace not implemented");
+    return optional<V>{};
+  }
+
+  int recover(bool simulated) {
+    errexit("Recover not implemented");
+    return 0;
   }
 };
 
-template<class T>
-class CLevelHashTableFactory : public RideableFactory {
-  Rideable* build(GlobalTestConfig *gtc) {
+template <class T> class CLevelHashTableFactory : public RideableFactory {
+  Rideable *build(GlobalTestConfig *gtc) {
     return new CLevelHashTable<T, T>(gtc->task_num);
   }
 };
