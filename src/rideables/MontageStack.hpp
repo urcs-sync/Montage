d6839e9df1582e2c05b27932e40d47b9ffb5ffe3
Shreif Elsaid <selsaid@node1x10a.cs.rochester.edu>
2021-01-09 11:58:44 -0500

    MontageStack compiles

--- src/rideables/MontageStack.hpp
+++ src/rideables/MontageStack.hpp
@@ -16,27 +16,36 @@ using namespace std;
 template <typename T>
 class MontageStack : public RStack<T>, Recoverable
 {
+public:
+    class Payload : public pds::PBlk{
+        GENERATE_FIELD(T, val, Payload);
+        GENERATE_FIELD(uint64_t, sn, Payload); 
+    public:
+        Payload(): pds::PBlk(){}
+        Payload(T v): pds::PBlk(), m_val(v), m_sn(0){}
+        Payload(const Payload& oth): pds::PBlk(oth), m_sn(0), m_val(oth.m_val){}
+        void persist(){}
+    };
 
 private:
-    struct StackNode
-    {
-        StackNode *next;
-        Payload *data;
-
+    struct StackNode{
         MontageStack *ds = nullptr;
+        pds::atomic_lin_var<StackNode *> *next;
+        Payload *payload;
+
+        StackNode(): next(nullptr), payload(nullptr){}
+        StackNode(MontageStack* ds_): ds(ds_), next(nullptr), payload(nullptr){}
+        StackNode(MontageStack* ds_, T v): ds(ds_), next(nullptr), payload(ds_->pnew<Payload>(v)){}
 
         void set_sn(uint64_t s)
         {
-            assert(data != nullptr && "payload shouldn't be null");
-            data->set_unsafe_sn(ds, s);
+            assert(payload != nullptr && "payload shouldn't be null");
+            payload->set_unsafe_sn(ds, s);
         }
-        StackNode(){};
-        StackNode(T v) : next(nullptr), data(pnew<Payload>(v)){};
-        ~StackNode()
-        {
-            if (data)
+        ~StackNode(){
+            if (payload)
             {
-                ds->preclaim(data);
+                ds->preclaim(payload);
             }
         }
     };
@@ -45,23 +54,12 @@ public:
     std::atomic<uint64_t> global_sn;
 
 private:
-    pds::atomic_lin_var<StackNode *> top;
+    pds::atomic_lin_var<StackNode*> top;
     RCUTracker<StackNode> tracker;
 
 public:
-    class Payload : public pds::PBlk
-    {
-        GENERATE_FIELD(T, val, Payload);
-        GENERATE_FIELD(uint64_t, sn, Payload);
 
-    public:
-        Payload() {}
-        Payload(T v, uint64_t n) : m_val(v), m_sn(n) {}
-        Payload(const Payload &oth) : PBlk(oth), m_sn(0), m_val(oth.m_val) {}
-        void persist() {}
-    };
     MontageStack(GlobalTestConfig* gtc) : Recoverable(gtc), global_sn(0), top(nullptr), tracker(gtc->task_num, 100, 1000, true) {}
-    ~MontageStack(){};
     
     void init_thread(GlobalTestConfig* gtc, LocalTestConfig* ltc){
         Recoverable::init_thread(gtc, ltc);
@@ -71,26 +69,24 @@ public:
         errexit("recover of MontageStack not implemented.");
         return 0;
     }
-    void push(T data, int tid);
+    ~MontageStack(){};
+
+    void push(T v, int tid);
     optional<T> pop(int tid);
     optional<T> peek(int tid);
     bool is_empty();
 };
 
 template <typename T>
-void MontageStack<T>::push(T data, int tid)
-{
+void MontageStack<T>::push(T v, int tid){
+    StackNode *new_node = new StackNode(this,v);
     tracker.start_op(tid);
-
-    StackNode *new_node = new StackNode(data);
-
-    StackNode *old_node;
 
     while(true)
     {
-        old_node = top.load();
         uint64_t s = global_sn.fetch_add(1);
-        new_node->next = old_node;
+        pds::lin_var old_node = top.load(this);
+        new_node->next->store(old_node);
         new_node->set_sn(s);
         begin_op();
         if(top.CAS_verify(this, old_node, new_node)){
@@ -107,29 +103,29 @@ void MontageStack<T>::push(T data, int tid)
 template <typename T>
 optional<T> MontageStack<T>::pop(int tid)
 {
-    tracker.start_op(tid);
-    StackNode *new_node;
-    StackNode *old_node;
     optional<T> res = {};
+    tracker.start_op(tid);
+    
     while(true)
     {
-        old_node = top.load();
-        if (old_node == NULL)
+        pds::lin_var old_node = top.load(this);
+        if (is_empty())
         {
             tracker.end_op(tid);
             return res;
         }
         begin_op();
-        new_node = old_node->next;
-        if (top.CAS_verify(this, old_node, new_node))){
-            res = (T)old_node->data->get_val(this);// old see new is impossible
-            pretire(old_node->data); 
+        StackNode* new_node = old_node.get_val<StackNode*>()->next->load_val(this);
+        if (top.CAS_verify(this, old_node, new_node)){
+            auto payload = old_node.get_val<StackNode*>()->payload;
+            res = (T)payload->get_val(this);// old see new is impossible
+            pretire(payload); 
+            tracker.retire(old_node.get_val<StackNode*>(), tid);
             end_op();
             break;
         }
         abort_op();
     };
-    tracker.retire(old_node, tid);
     tracker.end_op(tid);
     return res;
 }
@@ -138,18 +134,23 @@ optional<T> MontageStack<T>::peek(int tid)
 {
     tracker.start_op(tid);
     optional<T> res = {};
-    StackNode *top_node;
-    top_node = top.load();
-    tracker.end_op(tid);
-    auto data = top_node->data;
-    res = (T)data->get_val(this);
+    if(!is_empty()){
+
+        StackNode *top_node;
+        top_node = top.load(this);
+        tracker.end_op(tid);
+        auto payload = top_node->payload;
+        res = (T)payload->get_val(this);
+
+    }
+
     return res;
 }
 
 template <typename T>
 bool MontageStack<T>::is_empty()
 {
-    return top.load() == NULL;
+    return top.load_val(this) == nullptr;
 }
 
 template <class T>
@@ -157,7 +158,7 @@ class MontageStackFactory : public RideableFactory
 {
     Rideable *build(GlobalTestConfig *gtc)
     {
-        return new MontageStack<T>(gtc->task_num);
+        return new MontageStack<T>(gtc);
     }
 };
 
